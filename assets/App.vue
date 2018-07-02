001d7b0cd55c05e77f944217c235e31a3c12b32f
Aleksandr <aleksandrbartov@yandex.ru>
2018-07-02 22:29:22 +0400

    my first

---
+++ assets/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      fixed
+      class="indigo lighten-5"
+      app
+    >
+      <v-list>
+        <v-list-tile 
+          router
+          :to="item.to"
+          :key="i"
+          v-for="(item, i) in items"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed app :clipped-left="clipped" class="transparent">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+        <v-slide-y-transition mode="out-in">
+          <router-view></router-view>
+        </v-slide-y-transition>
+    </v-content>
+    <v-footer :fixed="fixed" app class="indigo accent-1">
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  import Meta from 'mixins/meta'
+
+  export default {
+    mixins: [Meta],
+
+    data () {
+      return {
+        clipped: false,
+        drawer: false,
+        fixed: false,
+        items: [
+          { icon: 'home', title: 'Главная', to: '/' },
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'иерей Александр Бартов'
+      }
+    }
+  }
+</script>
---
+++ pages/WelcomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-jumbotron
+    height="60vh"
+    :gradient="gradient"
+    dark
+    src="/static/priest_s.jpeg"
+  >
+    <v-container fill-height>
+      <v-layout align-center>
+        <v-flex text-xs-center>
+          <p class="title mx-0s" @click="changing">
+          Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future
+          Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future
+          Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future
+          </p>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-jumbotron>
+</template>
+
+
+<script>
+  export default {
+    data: () => ({
+      gradient: 'to top, rgba(197,202,233, 1) 30%, rgba(197,202,233, 0) 100%'
+    }),
+    methods: {
+      changing: function() {
+        this.gradient = 'to top, rgba(104,107,103, 1) 0%, rgba(104,107,103, 0) 100%'
+      }
+    }
+  }
+</script>
