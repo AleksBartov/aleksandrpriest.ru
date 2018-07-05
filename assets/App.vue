4aba56a651232fda76059f1c9fad0711d1af3156
Aleksandr <aleksandrbartov@yandex.ru>
2018-07-06 01:16:25 +0400

    yandex metrika have added script

--- assets/App.vue
+++ assets/App.vue
@@ -24,11 +24,67 @@
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
+
+      <v-list>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>home</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-title>Главная</v-list-tile-title>
+      </v-list-tile>
+
+      <v-list-group
+        prepend-icon="account_circle"
+      >
+        <v-list-tile slot="activator">
+          <v-list-tile-title>Таинства Церкви</v-list-tile-title>
+        </v-list-tile>
+
+        <v-list-group
+          no-action
+          sub-group
+        >
+          <v-list-tile slot="activator">
+            <v-list-tile-title>Крещение</v-list-tile-title>
+          </v-list-tile>
+
+          <v-list-tile
+            v-for="(admin, i) in admins"
+            :key="i"
+            @click=""
+          >
+            <v-list-tile-title v-text="admin[0]"></v-list-tile-title>
+            <v-list-tile-action>
+              <v-icon v-text="admin[1]"></v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list-group>
+
+        <v-list-group
+          sub-group
+          no-action
+        >
+          <v-list-tile slot="activator">
+            <v-list-tile-title>Исповедь</v-list-tile-title>
+          </v-list-tile>
+
+          <v-list-tile
+            v-for="(crud, i) in cruds"
+            :key="i"
+            @click=""
+          >
+            <v-list-tile-title v-text="crud[0]"></v-list-tile-title>
+            <v-list-tile-action>
+              <v-icon v-text="crud[1]"></v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list-group>
+      </v-list-group>
+    </v-list>
       
       <v-divider></v-divider>
       <v-spacer></v-spacer>
       <v-divider></v-divider>
-
 
 
       <v-flex xs8 offset-xs2 mt-5>
@@ -171,6 +227,16 @@
         ],
         links: [
           'поддержать проект'
+        ],
+        admins: [
+          ['Хочу крестить ребенка', 'people_outline'],
+          ['Просят быть крестным', 'settings']
+        ],
+        cruds: [
+          ['Create', 'add'],
+          ['Read', 'insert_drive_file'],
+          ['Update', 'update'],
+          ['Delete', 'delete']
         ],
         miniVariant: false,
         right: true,
