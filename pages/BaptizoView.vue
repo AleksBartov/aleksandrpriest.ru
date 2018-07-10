18a4d9f6630727bc2d1a47e28afb51165fc1d5ec
Aleksandr <aleksandrbartov@yandex.ru>
2018-07-11 03:03:22 +0400

    trying to change logic

--- pages/BaptizoView.vue
+++ pages/BaptizoView.vue
@@ -4,6 +4,7 @@
     <div class="headline">Крещение детей</div>
     <div>Чтобы лучше понять вас и дать оптимальный ответ, пожалуйста, выберите самый подходящий к вашему случаю вариант</div>
   </v-card-title>
+
 <v-stepper v-model="e6" vertical>
     <v-stepper-step :complete="e6 > 1" step="1">
       Я крещу детей потому что я...
@@ -12,30 +13,27 @@
     <v-stepper-content step="1">
       <v-card color="grey lighten-1" class="mb-5" height="auto">
         <v-container fluid>
-          <v-radio-group v-model="values">
-            <v-radio color="success" label="человек верующий, регулярно посещаю богослужения и учавствую в святых Таинствах. Хочу, чтоб и мои дети были членами Церкви" value="Выбрали ли вы крестных родителей ( по крайней мере, один крестный одного с крещаемым пола )"></v-radio>
+          <v-radio-group v-model="first">
+            <v-radio color="success" :label="firstQuestion.firstAnswer" :value="firstQuestion.secondQuestion1"></v-radio>
             <v-divider class="mt-3 mb-3"></v-divider>
-            <v-radio color="info" label="верю в Бога, считаю себя православным. В храм захожу довольно часто, но исповедуюсь и причащаюсь пару раз в году. Считаю, что, как крестили в детстве меня, так и я должен крестить своих детей, чтоб Господь хранил их" value="radio-2"></v-radio>
+            <v-radio color="info" :label="firstQuestion.secondAnswer" :value="firstQuestion.secondQuestion2"></v-radio>
             <v-divider class="mt-3 mb-3"></v-divider>
-            <v-radio color="warning" label="в Бога я верю и Церковь для меня важна. Вхраме бываю редко. Не исповедывался давно. Поскольку жуву в России, считаю, что и вера должна быть русской, православной. По-традиции крещу и своих детей" value="radio-3"></v-radio>
-            <v-divider class="mt-3 mb-3"></v-divider>
-            <v-radio color="error" label="хоть и не нуждаюсь в Церкви но детей крещу, поскольку все родственники меня в этом убеждают" value="radio-4"></v-radio>                  
+            <v-radio color="warning" :label="firstQuestion.thirdAnswer" :value="firstQuestion.secondQuestion3"></v-radio>
           </v-radio-group>
         </v-container>
       </v-card>
       <v-btn color="primary" @click="e6 = 2">Далее</v-btn>
     </v-stepper-content>
 
-    <v-stepper-step :complete="e6 > 2" step="2">{{values}}</v-stepper-step>
+    <v-stepper-step :complete="e6 > 2" step="2">{{ selected }}</v-stepper-step>
 
     <v-stepper-content step="2">
       <v-card color="grey lighten-1" class="mb-5" height="auto">
         <v-container fluid>
-          <v-radio-group :mandatory="false">
-            <v-radio color="success" label="" value="radio-1"></v-radio>
-            <v-radio color="info" label="" value="radio-2"></v-radio>
-            <v-radio color="warning" label="" value="radio-3"></v-radio>
-            <v-radio color="error" label="" value="radio-4"></v-radio>                  
+          <v-radio-group v-model="second">
+            <v-radio color="success" :label="secondQuestionOne.firstAnswer" :value="secondQuestionOne.thirdQuestion1"></v-radio>
+            <v-radio color="info" :label="secondQuestionOne.secondAnswer" :value="secondQuestionOne.thirdQuestion2"></v-radio>
+            <v-radio color="warning" :label="secondQuestionOne.thirdAnswer" :value="secondQuestionOne.thirdQuestion3"></v-radio>
           </v-radio-group>
         </v-container>
       </v-card>
@@ -43,12 +41,12 @@
       <v-btn flat @click="e6 = 1">Отмена</v-btn>
     </v-stepper-content>
 
-    <v-stepper-step :complete="e6 > 3" step="3">{{question_3}}</v-stepper-step>
+    <v-stepper-step :complete="e6 > 3" step="3">{{ selectedForThree }}</v-stepper-step>
 
     <v-stepper-content step="3">
       <v-card color="grey lighten-1" class="mb-5" height="auto">
         <v-container fluid>
-          <v-radio-group :mandatory="false">
+          <v-radio-group v-model="values">
             <v-radio color="success" label="верую во Святую Троицу и хочу, чтоб мои дети также были в Церкви" value="radio-1"></v-radio>
             <v-radio color="info" label="" value="radio-2"></v-radio>
             <v-radio color="warning" label="" value="radio-3"></v-radio>
@@ -60,21 +58,6 @@
       <v-btn flat @click="e6 = 2">Отмена</v-btn>
     </v-stepper-content>
 
-    <v-stepper-step step="4">{{question_4}}</v-stepper-step>
-    <v-stepper-content step="4">
-      <v-card color="grey lighten-1" class="mb-5" height="auto">
-        <v-container fluid>
-          <v-radio-group :mandatory="false">
-            <v-radio color="success" label="верую во Святую Троицу и хочу, чтоб мои дети также были в Церкви" value="radio-1"></v-radio>
-            <v-radio color="info" label="" value="radio-2"></v-radio>
-            <v-radio color="warning" label="" value="radio-3"></v-radio>
-            <v-radio color="error" label="" value="radio-4"></v-radio>                  
-          </v-radio-group>
-        </v-container>
-      </v-card>
-      <v-btn color="primary" @click="e6 = 1">Далее</v-btn>
-      <v-btn flat @click="e6 = 3">Отмена</v-btn>
-    </v-stepper-content>
   </v-stepper>
 </v-card>
 </template>
@@ -83,11 +66,43 @@
   export default {
     data () {
       return {
+        firstQuestion: {
+          firstAnswer: 'я хороший',
+          secondQuestion1: 'ты уверен?',
+          secondAnswer: 'я хуже первого',
+          secondQuestion2: 'почему ты так решил?',
+          thirdAnswer: 'я плохой',
+          secondQuestion3: 'кто тебе сказал такую глупость?',
+        },
+        secondQuestionOne: {
+          firstAnswer: 'Конечно!',
+          thirdQuestion1: 'может ты в прелести?',
+          secondAnswer: 'Не совсем',
+          thirdQuestion2: 'ну и как нам разобраться?',
+          thirdAnswer: 'нет, я вру сам себе',
+          thirdQuestion3: 'а ты спросил маму?'
+        },
+        secondQuestionTwo: {
+          firstAnswer: 'мне сказали близкие',
+          secondAnswer: 'я видел его и слышал',
+          thirdAnswer: 'я не хожу в храм, а он ходит'
+        },
+        secondQuestionThree: {
+          firstAnswer: 'жена',
+          secondAnswer: 'начальник',
+          thirdAnswer: 'совесть'
+        },
         e6: 1,
-        values: 'Выбрали ли вы крестных родителей ( по крайней мере, один крестный одного с крещаемым пола )',
-        question_2: 'Вопрос номер два',
-        question_3: 'Вопрос номер три',
-        question_4: 'Заключительный вопрос'
+        first: 'ты уверен?',
+        second: 'может ты в прелести?'
+      }
+    },
+    computed: {
+      selected: function() {
+        return this.first
+      },
+      selectedForThree: function() {
+        return this.second
       }
     }
   }
